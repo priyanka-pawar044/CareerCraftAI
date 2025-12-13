c7235654bc3598d7d9b4f197de962894712cdea8
Priyanka Pawar <priyankapawar7764@gmail.com>
2025-12-13 14:13:35 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,10 +5,10 @@
 // guide to connect to a real Firebase project.
 
 export const firebaseConfig = {
-  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "example.firebaseapp.com",
-  projectId: "example",
-  storageBucket: "example.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:0000000000000000000000",
+  "projectId": "studio-8555919986-7f9c9",
+  "appId": "1:1014097693518:web:5e5ad301316269effaac1f",
+  "apiKey": "AIzaSyBv8pjOZbkaO4yJateq7aZjmCFUsYwbZIs",
+  "authDomain": "studio-8555919986-7f9c9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1014097693518"
 };
