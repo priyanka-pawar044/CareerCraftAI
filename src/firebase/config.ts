41f17072beb964fe0f4b8c4bd08c81f5740a7bed
Priyanka Pawar <priyankapawar7764@gmail.com>
2025-12-19 05:30:18 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/api

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-8555919986-7f9c9",
-  "appId": "1:1014097693518:web:a0d189f36f481c69faac1f",
+  "appId": "1:1014097693518:web:79b0c950a7c93c4e0b04c2",
   "storageBucket": "studio-8555919986-7f9c9.appspot.com",
-  "apiKey": "AIzaSyCVQ0kfhgqxX7Uc2B1f5PSiAbhAXPj-YFk",
+  "apiKey": "AIzaSyB5zES4RoTA4_drv2lPXd2IITd9fYJizZw",
   "authDomain": "studio-8555919986-7f9c9.firebaseapp.com",
   "messagingSenderId": "1014097693518"
 };
