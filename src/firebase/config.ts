67a1cb1f9de80487938104a5ab737b4b3c7dc752
Priyanka Pawar <priyankapawar7764@gmail.com>
2025-12-19 05:27:23 +0000

    AIzaSyB5zES4RoTA4_drv2lPXd2IITd9fYJizZw

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "careerpilot-ai-app",
   "appId": "1:654128521013:web:a78be6f190a6c0c27258e7",
   "storageBucket": "careerpilot-ai-app.appspot.com",
-  "apiKey": "AIzaSyCVfVxxmhw1yxda5T1tQVWx_LwQUrEW4-I",
+  "apiKey": "AIzaSyB5zES4RoTA4_drv2lPXd2IITd9fYJizZw",
   "authDomain": "careerpilot-ai-app.firebaseapp.com",
   "messagingSenderId": "654128521013"
 };
