108baafb9aadbe05cfcc0db12d55210d966a4252
Priyanka Pawar <priyankapawar7764@gmail.com>
2025-12-20 09:10:39 +0000

    AIzaSyA6D4tkR6dFHPQrjsOTnThekakte3P5eWM use this api key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-8555919986-e822a",
   "appId": "1:1014097693518:web:b8c950a2559a43a08d2035",
   "storageBucket": "studio-8555919986-e822a.appspot.com",
-  "apiKey": "AIzaSyAN5Ukp369ZiKE8zl7wUFea7yIeC0FofjM",
+  "apiKey": "AIzaSyA6D4tkR6dFHPQrjsOTnThekakte3P5eWM",
   "authDomain": "studio-8555919986-e822a.firebaseapp.com",
   "messagingSenderId": "1014097693518"
 };
