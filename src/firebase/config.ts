f4d3fd2e9a8faef0e4ea789845f885256dfd5186
Priyanka Pawar <priyankapawar7764@gmail.com>
2025-12-20 07:24:36 +0000

    AIzaSyAN5Ukp369ZiKE8zl7wUFea7yIeC0FofjM

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-8555919986-7f9c9",
   "appId": "1:1014097693518:web:79b0c950a7c93c4e0b04c2",
   "storageBucket": "studio-8555919986-7f9c9.appspot.com",
-  "apiKey": "AIzaSyB5zES4RoTA4_drv2lPXd2IITd9fYJizZw",
+  "apiKey": "AIzaSyAN5Ukp369ZiKE8zl7wUFea7yIeC0FofjM",
   "authDomain": "studio-8555919986-7f9c9.firebaseapp.com",
   "messagingSenderId": "1014097693518"
 };
